4345f3bafdde74c042fa4454529e3d92b7d801d9
ChristineShaffer <shaffercv@gmail.com>
2020-09-18 17:36:05 -0600

    Added authenticated middleware and an index page which verifies authentication.

---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    middleware: "authenticated"
+  };
+</script>
+
+<style>
+
+</style>
--- frontend/pages/videos/index.vue
+++ frontend/pages/videos/index.vue
@@ -21,6 +21,7 @@
 
 <script>
 export default {
+  middleware: "authenticated",
   data() {
     return {
       form: {
