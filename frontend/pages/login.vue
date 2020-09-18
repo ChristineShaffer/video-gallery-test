1b68e0e285e05ff0d72ccb647c057c99f7ca9311
ChristineShaffer <shaffercv@gmail.com>
2020-09-18 09:57:01 -0600

    Changed button colour to AlgaeCal green on login page.

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -18,7 +18,7 @@
               placeholder="Enter email"
             ></b-form-input>
           </b-form-group>
-          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="submit" class="button--green">Login</b-button>
         </b-form>
       </b-card>
     </div>
