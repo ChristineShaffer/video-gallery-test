c7798a0844c92220fbfd4c08e1b3f324fe4af44a
ChristineShaffer <shaffercv@gmail.com>
2020-09-18 17:31:20 -0600

    Moved login form to LoginForm component.

---
+++ frontend/components/LoginForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <b-card class="login-card">
+    <h1 class="title">User Login</h1>
+
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.username"
+          type="text"
+          minlength="3"
+          required
+          placeholder="Enter Your Username"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" :disabled="form.username.length < 3" class="button--green" id="login-button">
+        Login
+      </b-button>
+    </b-form>
+  </b-card>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          username: ""
+        }
+      };
+    },
+    methods: {
+      onSubmit(event){
+        console.log("A form was submitted");
+        event.preventDefault();
+      }
+    }
+  }
+</script>
+
+<style>
+  #login-button {
+    margin-top: 2em;
+  }
+
+  .login-card {
+    padding: 2em;
+  }
+
+  .title {
+    font-family:
+      "Quicksand",
+      "Source Sans Pro",
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      "Helvetica Neue",
+      Arial,
+      sans-serif;
+    display: block;
+    font-weight: bold;
+    font-size: 2em;
+    color: #3b8070;
+    letter-spacing: 1px;
+    padding: 1em;
+  }
+</style>
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,44 +1,11 @@
 <template>
   <div class="container">
-    <div>
-      <b-card class="login-card">
-        <h1 class="title">User Login</h1>
-
-        <b-form @submit="onSubmit">
-          <b-form-group
-            id="input-group-1"
-            label-for="input-1"
-          >
-            <b-form-input
-              id="input-1"
-              v-model="form.email"
-              type="email"
-              required
-              placeholder="Your Email Address"
-            ></b-form-input>
-          </b-form-group>
-          <b-button type="submit" class="button--green" id="login-button">Login</b-button>
-        </b-form>
-      </b-card>
-    </div>
+    <LoginForm />
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      form: {
-        email: ''
-      },
-    };
-  },
-  methods: {
-    onSubmit(){
-      console.log('A form was submitted');
-    },
-  }
-}
+
 </script>
 
 <style>
@@ -53,44 +20,5 @@ export default {
     justify-content: center;
     align-items: center;
     text-align: center;
-  }
-
-  .title {
-    font-family:
-      "Quicksand",
-      "Source Sans Pro",
-      -apple-system,
-      BlinkMacSystemFont,
-      "Segoe UI",
-      Roboto,
-      "Helvetica Neue",
-      Arial,
-      sans-serif;
-    display: block;
-    font-weight: bold;
-    font-size: 2em;
-    color: #3b8070;
-    letter-spacing: 1px;
-    padding: 1em;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
-
-  .login-card {
-    padding: 2em;
-  }
-
-  #login-button {
-    margin-top: 2em;
   }
 </style>
